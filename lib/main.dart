aa3223e9e4deb248e21e84b0432318e28c48db73
ValentinVignal <valentin.vignal.dev@outlook.fr>
2021-04-02 19:32:14 +0800

    New PR, action v0.3

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
  
 var myVar;
 
+var my2ndVar;
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
---
+++ lib/screens/other_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'dart:math';
+
+class OtherScreen extends StatefulWidget {
+  @override
+  _OtherScreenState createState() => _OtherScreenState();
+}
+
+class _OtherScreenState extends State<OtherScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
